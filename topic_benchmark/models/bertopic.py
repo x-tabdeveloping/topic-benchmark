6c74ec3b853545b92b4f77fc08c1c2508c59c255
Márton Kardos <power.up1163@gmail.com>
2024-02-27 09:29:26 +0100

    Added Turftopic implementation of BERTopic

--- topic_benchmark/models/bertopic.py
+++ topic_benchmark/models/bertopic.py
@@ -1,81 +1,30 @@
-from functools import partial
-from typing import Optional
-
-import numpy as np
-from bertopic import BERTopic
+from hdbscan import HDBSCAN
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.preprocessing import label_binarize
-from turftopic.data import TopicData
+from turftopic import ClusteringTopicModel
+from umap.umap_ import UMAP
 
-from topic_benchmark.base import Loader, TopicModel
+from topic_benchmark.base import Loader
 from topic_benchmark.registries import model_registry
-
-
-class BERTopicWrapper(TopicModel):
-    """Wrapper for BERTopic models to be used in topicwizard."""
-
-    def __init__(self, model):
-        self.model = model
-
-    def prepare_topic_data(
-        self, corpus: list[str], embeddings: Optional[np.ndarray] = None
-    ) -> TopicData:
-        from bertopic.backend._utils import select_backend
-
-        if embeddings is None:
-            self.model.embedding_model = select_backend(
-                self.model.embedding_model, language=self.model.language
-            )
-            embeddings = self.model._extract_embeddings(
-                corpus,
-                method="document",
-            )
-        if self.model.c_tf_idf_ is None:
-            topic_labels, _ = self.model.fit_transform(
-                corpus, embeddings=embeddings
-            )
-        else:
-            topic_labels, _ = self.model.transform(
-                corpus, embeddings=embeddings
-            )
-        document_topic_matrix = label_binarize(
-            topic_labels, classes=self.model.topics_
-        )
-        document_term_matrix = self.model.vectorizer_model.transform(corpus)
-        vocab = self.model.vectorizer_model.get_feature_names_out()
-        if self.model.topic_labels_:
-            topic_names = [
-                self.model.topic_labels_[topic] for topic in self.model.topics_
-            ]
-        else:
-            topic_names = self.model.generate_topic_labels(nr_words=3)
-
-        def transform(corpus: list[str]):
-            topic_labels, _ = self.model.transform(
-                corpus, embeddings=embeddings
-            )
-            return label_binarize(topic_labels, classes=self.model.topics_)
-
-        return TopicData(
-            corpus=corpus,
-            vocab=vocab,
-            document_term_matrix=document_term_matrix,
-            document_topic_matrix=np.asarray(document_topic_matrix),
-            topic_term_matrix=self.model.c_tf_idf_.toarray(),
-            document_representation=embeddings,  # type: ignore
-            transform=transform,
-            topic_names=topic_names,
-        )
 
 
 @model_registry.register("BERTopic")
 def load_bertopic(encoder, vectorizer: CountVectorizer) -> Loader:
+    dim_red = UMAP(n_neighbors=15, n_components=5, metric="cosine")
+    clustering = HDBSCAN(
+        min_cluster_size=15,
+        metric="euclidean",
+        cluster_selection_method="eom",
+    )
+
     def _load(n_components: int):
-        model = BERTopic(
-            embedding_model=encoder,
-            vectorizer_model=vectorizer,
-            nr_topics=n_components,
+        return ClusteringTopicModel(
+            encoder=encoder,
+            vectorizer=vectorizer,
+            dimensionality_reduction=dim_red,
+            clustering=clustering,
+            feature_importance="c-tf-idf",
+            reduction_method="agglomerative",
+            n_reduce_to=n_components,
         )
-        return BERTopicWrapper(model)
 
     return _load
