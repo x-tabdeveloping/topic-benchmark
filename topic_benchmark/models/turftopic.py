fa07eb2869900edfaedd2559991c1b16c923b951
Márton Kardos <power.up1163@gmail.com>
2024-03-07 14:12:58 +0100

    Commented out KeyNMF and GMM from registry

--- topic_benchmark/models/turftopic.py
+++ topic_benchmark/models/turftopic.py
@@ -1,28 +1,25 @@
 from functools import partial
 
 from sklearn.feature_extraction.text import CountVectorizer
-from turftopic import (GMM, AutoEncodingTopicModel, KeyNMF,
-                       SemanticSignalSeparation)
+from turftopic import GMM, AutoEncodingTopicModel, KeyNMF, SemanticSignalSeparation
 
 from topic_benchmark.base import Loader
 from topic_benchmark.registries import model_registry
 
 
-@model_registry.register("GMM")
+# @model_registry.register("GMM")
 def load_gmm(encoder, vectorizer: CountVectorizer) -> Loader:
     return partial(GMM, encoder=encoder, vectorizer=vectorizer)
 
 
-@model_registry.register("KeyNMF")
+# @model_registry.register("KeyNMF")
 def load_keynmf(encoder, vectorizer: CountVectorizer) -> Loader:
     return partial(KeyNMF, encoder=encoder, vectorizer=vectorizer)
 
 
 @model_registry.register("S³")
 def load_s3(encoder, vectorizer: CountVectorizer) -> Loader:
-    return partial(
-        SemanticSignalSeparation, encoder=encoder, vectorizer=vectorizer
-    )
+    return partial(SemanticSignalSeparation, encoder=encoder, vectorizer=vectorizer)
 
 
 @model_registry.register("CombinedTM")
