787b222b5393366d64039e2afdebcd151df9b634
Márton Kardos <power.up1163@gmail.com>
2024-05-23 16:10:47 +0200

    Added error bars to table and abstracted away from the concrete number of metrics

--- topic_benchmark/table.py
+++ topic_benchmark/table.py
@@ -1,24 +1,31 @@
 from typing import Union
 
+import numpy as np
 import pandas as pd
 
 from topic_benchmark.benchmark import BenchmarkEntry, BenchmarkError
 
+METRICS_TO_DISPLAY_NAME = {
+    "NPMI Coherence": "NPMI",
+    "Word Embedding Coherence": "WEC",
+    "Diversity": "Diversity",
+}
+
+METRICS = ["NPMI", "WEC", "Diversity"]
+
 
 def produce_header(datasets: list[str]) -> list[str]:
     n_datasets = len(datasets)
-    column_align = "l" + ("c" * (n_datasets * 3))
+    column_align = "l" + ("c" * (n_datasets * len(METRICS)))
     midrules = []
     for i in range(n_datasets):
-        start = (i + 1) * 3 - 1
-        end = start + 2
+        start = (i + 1) * len(METRICS) - 2
+        end = start + len(METRICS) - 1
         midrule = f"\\cmidrule(l{{3pt}}r{{3pt}}){{{start}-{end}}}"
         midrules.append(midrule)
-    metric_names = " & " + " & ".join(
-        ["NPMI", "WEC", "Diversity"] * n_datasets
-    )
+    metric_names = " & " + " & ".join(METRICS * n_datasets)
     lines = [
-        "\\begin{table}[h]",
+        "\\begin{table*}[p]",
         "\\bgroup",
         "\\resizebox{\\textwidth}{!}{",
         "\\def\\arraystretch{1.3}",
@@ -26,7 +33,7 @@ def produce_header(datasets: list[str]) -> list[str]:
         "\\toprule",
         " & "
         + " & ".join(
-            f"\\multicolumn{{3}}{{c}}{{\\textbf{{{dataset}}}}}"
+            f"\\multicolumn{{{len(METRICS)}}}{{c}}{{\\textbf{{{dataset}}}}}"
             for dataset in datasets
         )
         + "\\\\",
@@ -45,33 +52,60 @@ def produce_footer() -> list[str]:
         "}",
         "\\egroup",
         "\\caption{ Coherence and Diversity of Topics over Multiple Datasets \\\\",
-        "\\textit{Best in bold, second best underlined}",
+        "\\textit{Best in bold, second best underlined, uncertainty represents $2 \\cdot SD$}",
         "}",
-        "\\end{table}",
+        "\\end{table*}",
     ]
 
 
+def mean_with_uncertainty(s: pd.Series) -> tuple[float, float]:
+    """Returns mean with 2*standard deviation."""
+    return s.mean(), 2 * np.std(s)
+
+
+def format_value(val: tuple[float, float]) -> str:
+    m, error = val
+    if m < 1:
+        error_str = f"{error:.2f}".lstrip("0")
+        m_str = f"{m:.2f}"
+    else:
+        error_str = f"{error:.0f}"
+        m_str = f"{m:.0f}"
+    return f"{m_str} ± \\textit{{{error_str}}}"
+
+
 def format_cells(table: pd.DataFrame) -> pd.DataFrame:
-    table = table[["NPMI Coherence", "Word Embedding Coherence", "Diversity"]]
-    bold = {column: table[column].max() for column in table.columns}
-    underline = {
-        column: table[column].nlargest(n=2).iloc[-1]
-        for column in table.columns
-    }
+    table = table[METRICS]
+    bold = dict()
+    underline = dict()
+    for column in table.columns:
+        if column != "runtime_s":
+            best_val = table[column].map(lambda val: val[0]).max()
+        else:
+            best_val = table[column].map(lambda val: val[0]).min()
+        bold[column] = best_val
+        if column != "runtime_s":
+            second_best = (
+                table[column].map(lambda val: val[0]).nlargest(n=2).iloc[-1]
+            )
+        else:
+            second_best = (
+                table[column].map(lambda val: val[0]).nsmallest(n=2).iloc[-1]
+            )
+        bold[column] = best_val
+        underline[column] = second_best
     records = []
     for model, row in table.iterrows():
         record = {}
         for column, value in row.items():
-            formatted_value = f"{value:.3f}"
+            formatted_value = format_value(value)
             record[column] = formatted_value
-            if value == bold[column]:
+            if value[0] == bold[column]:
                 record[column] = f"\\textbf{{{formatted_value}}}"
-            if value == underline[column]:
+            if value[0] == underline[column]:
                 record[column] = f"\\underline{{{formatted_value}}}"
         records.append(record)
-    return pd.DataFrame.from_records(records, index=table.index)[
-        ["NPMI Coherence", "Word Embedding Coherence", "Diversity"]
-    ]
+    return pd.DataFrame.from_records(records, index=table.index)[METRICS]
 
 
 MODEL_ORDER = [
@@ -129,19 +163,20 @@ def produce_encoder_rows(
     entries = [entry for entry in entries if "error_message" not in entry]
     data = pd.DataFrame.from_records(entries)
     data = data.join(pd.json_normalize(data["results"]))
-    data = data.groupby(["dataset", "model"])[
-        [
-            "NPMI Coherence",
-            "Word Embedding Coherence",
-            "Diversity",
-        ]
-    ].mean()
+    data = data.rename(columns=METRICS_TO_DISPLAY_NAME)
+    data = data.groupby(["dataset", "model"])[METRICS].agg(
+        mean_with_uncertainty
+    )
     groups = dict(list(data.groupby("dataset")))
     # Loading tables in proper order
     group_tables = [groups[dataset] for dataset in DATASET_ORDER]
+    n_datasets = len(DATASET_ORDER)
+    n_metrics = len(METRICS)
     lines = [
         "\\midrule",
-        f"\\textbf{{{encoder_name}}} & & & & & & \\\\",
+        f"\\textbf{{{encoder_name}}} "
+        + " ".join(["&"] * (n_datasets * n_metrics))
+        + "\\\\",
         "\\midrule",
         *produce_body(group_tables),
     ]
