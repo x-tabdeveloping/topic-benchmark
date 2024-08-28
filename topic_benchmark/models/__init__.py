9ad0d5b071f44d6f7648499baaff1ea932960fe9
Márton Kardos <power.up1163@gmail.com>
2024-08-28 15:22:19 +0200

    Added ECRTM to init file

--- topic_benchmark/models/__init__.py
+++ topic_benchmark/models/__init__.py
@@ -2,3 +2,4 @@ from topic_benchmark.models.bertopic import *
 from topic_benchmark.models.classical import *
 from topic_benchmark.models.top2vec import *
 from topic_benchmark.models.turftopic import *
+from topic_benchmark.models.ecrtm import *
