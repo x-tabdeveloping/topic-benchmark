009d8930bc43deeb1e8ec5b61b096378cd134c3f
Márton Kardos <power.up1163@gmail.com>
2024-09-04 13:30:12 +0200

    New datasets get imported now and added to the registry

--- topic_benchmark/datasets/__init__.py
+++ topic_benchmark/datasets/__init__.py
@@ -1,3 +1,5 @@
 from topic_benchmark.datasets.arxiv_ml import *
 from topic_benchmark.datasets.bbc_news import *
 from topic_benchmark.datasets.newsgroups import *
+from topic_benchmark.datasets.stackexchange import *
+from topic_benchmark.datasets.wiki_medical import *
